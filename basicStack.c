8016c09159fb77ba730c1595b6fef662e19a6013
Khush Virmani <khushvirmani@gmail.com>
2019-11-12 17:16:20 +0530

    Adding basic stack implementation.

--- basicStack.c
+++ basicStack.c
@@ -2,10 +2,61 @@
 #include<stdlib.h>
 #define MAX_SIZE 100
 int stack1[MAX_SIZE];
-int top=-1;  // initial value
-void Push(int x){
+int minStack[MAX_SIZE];
+int top1=-1;  // initial value
+int top2=-1;
+void push(int* stack, int* top, int x){
+    stack[(*top) +1]=x;
+    *top+=1;
 
+    if(top2==-1){
+        minStack[top2 +1]=x;
+        top2+=1;
+    } 
+    else{
+        if(minStack[top2]>x){
+            minStack[top2 +1]=x;
+            top2+=1;
+        }
+        else{
+            minStack[top2 +1]=minStack[top2];
+            top2+=1;
+        }
+    }
+}
+int pop(int* stack, int* top){
+    int pop_elem;
+    pop_elem = stack[*top];
+    *top-=1;
+    top2-=1;
+    return pop_elem;    
+}
+int minStackVal(){
+    pop(minStack, &top2);
 }
 
+void stackPrinter(int* stack, int* top){
+    int idx;
+    for(idx=*top;idx>=0;idx--){
+        printf("%d\n",stack[idx]);
+    }
+}
 void main(){
+    int num;
+    int popped;
+    printf("Enter 5 numbers:\n");
+    for(int kx=0;kx<5;kx++){
+    scanf("%d", &num);
+    push(stack1, &top1, num);
+    }
+    printf("\nPrinting stack1.\n");
+    stackPrinter(stack1, &top1);
+    printf("\nPrinting stack1 after popping a value.\n");
+    popped = pop(stack1, &top1);
+    stackPrinter(stack1, &top1);
+    printf("\nPrinting minStack.\n");
+    stackPrinter(minStack, &top2);
+    int min_val;
+    min_val = minStackVal();
+    printf("\n%d\n", min_val);
 }
--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -8,24 +8,36 @@ struct Node{
 };
 
 void insert_at_end(struct Node* head_node, int val){
-    struct Node* temp;
-    temp = head_node;
-    struct Node* temp_node;
-    temp_node = (struct Node*)malloc(1*sizeof(struct Node));
-    temp_node->data = val;
-    temp_node->next = NULL;
-    while(temp->next!=NULL){
-        temp = temp->next;
+    if(head_node->data==0){
+        head_node->data = val;
     }
-    temp_node->prev = temp;
-    temp->next=temp_node;
+    else{
+        struct Node* temp;
+        temp = head_node;
+        struct Node* temp_node;
+        temp_node = (struct Node*)malloc(1*sizeof(struct Node));
+        temp_node->data = val;
+        temp_node->next = NULL;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp_node->prev = temp;
+        temp->next=temp_node;
+    }
+}
+void delete__node(struct Node* head_node, int position, int data_val){
+
+}
+
+void sort_dll(struct Node* head_node){
+
 }
 
 void print_dll(struct Node* head_node){
     struct Node* tmp;    
     tmp = head_node;
     while(tmp!=NULL){
-        printf("%d\t%p\t%p\t%p\n", tmp->data, tmp->prev, tmp, tmp->next);
+        printf("%d\t%p\t%p\t%p\n", tmp->data, tmp->prev, tmp,  tmp->next);
         tmp = tmp->next;
     }
 }
@@ -34,7 +46,7 @@ void main(){
     struct Node* head1;
     head1 = (struct Node*)malloc(1* sizeof(struct Node));
     head1->prev=NULL;
-    head1->data=1;
+    head1->data= 0;       // for initializing !
     head1->next = NULL;
     int ix, n, num_nodes;
     printf("Enter number of nodes to be added : ");
